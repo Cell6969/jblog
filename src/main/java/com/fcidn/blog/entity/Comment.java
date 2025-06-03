95a4750f92a7bc06181ea57000e21fcd8b112f00
Cell6969 <bossmarinoo@gmail.com>
2025-06-03 11:03:32 +0700

    add: comment entity and controller

---
+++ src/main/java/com/fcidn/blog/entity/Comment.java
@@ -0,0 +1,13 @@
+package com.fcidn.blog.entity;
+
+import lombok.Data;
+
+@Data
+public class Comment {
+    private Integer id;
+    private String name;
+    private String email;
+    private Integer postId;
+    private String body;
+    private Long createdAt;
+}
