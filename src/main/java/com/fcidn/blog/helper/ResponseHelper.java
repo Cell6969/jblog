916e5e46e26568f0df1166510544cbdaecfea451
Cell6969 <bossmarinoo@gmail.com>
2025-06-05 15:46:38 +0700

    add: helper for api response

---
+++ src/main/java/com/fcidn/blog/helper/ResponseHelper.java
@@ -0,0 +1,14 @@
+package com.fcidn.blog.helper;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+public class ResponseHelper {
+    public static <T> ResponseEntity<ApiResponse<T>> response(T data, HttpStatus httpStatus, String message) {
+        return ResponseEntity.ok(ApiResponse.<T>builder()
+                        .code(httpStatus.value())
+                        .message(message)
+                        .data(data)
+                        .build());
+    }
+}
