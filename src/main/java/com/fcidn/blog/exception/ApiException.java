9c5c4f080a5b518d985d8f0573e683b515189410
Cell6969 <bossmarinoo@gmail.com>
2025-06-05 11:30:00 +0700

    add: case exception

--- src/main/java/com/fcidn/blog/exception/ApiException.java
+++ src/main/java/com/fcidn/blog/exception/ApiException.java
@@ -4,16 +4,9 @@ import org.springframework.http.HttpStatus;
 
 public class ApiException extends RuntimeException {
     private final HttpStatus httpStatus;
-    private final String message;
     public ApiException(String message, HttpStatus httpStatus) {
         super(message);
-        this.message = message;
         this.httpStatus = httpStatus;
-    }
-
-    @Override
-    public String getMessage() {
-        return message;
     }
 
     public HttpStatus getHttpStatus() {
--- src/main/java/com/fcidn/blog/exception/ApiExceptionHandler.java
+++ src/main/java/com/fcidn/blog/exception/ApiExceptionHandler.java
@@ -1,10 +1,13 @@
 package com.fcidn.blog.exception;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.MissingServletRequestParameterException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
@@ -12,12 +15,40 @@ import java.util.List;
 public class ApiExceptionHandler {
     @ExceptionHandler(ApiException.class)
     public ResponseEntity<ApiExceptionResponse> handler(ApiException exception) {
-        List<String> errorMessages = new ArrayList<>(Collections.singletonList(exception.getMessage()));
-        Integer statusCode = exception.getHttpStatus().value();
+        return buildResponse(exception.getHttpStatus(), Collections.singletonList(exception.getMessage()));
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ApiExceptionResponse> handleValidationException(MethodArgumentNotValidException exception) {
+        List<String> messages = exception.getBindingResult()
+                .getFieldErrors()
+                .stream()
+                .map(err -> err.getField() + ": " + err.getDefaultMessage())
+                .toList();
+        return buildResponse(HttpStatus.BAD_REQUEST, messages);
+    }
+
+    @ExceptionHandler(MissingServletRequestParameterException.class)
+    public ResponseEntity<ApiExceptionResponse> handleMissingParameters(MissingServletRequestParameterException exception) {
+        String messages = "Missing required parameters: " + exception.getParameterName();
+        return buildResponse(HttpStatus.BAD_REQUEST, Collections.singletonList(messages));
+    }
+
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    public ResponseEntity<ApiExceptionResponse> handleInvalidJson(HttpMessageNotReadableException exception) {
+        return buildResponse(HttpStatus.BAD_REQUEST, Collections.singletonList("Malformed or unreadable JSON request"));
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ApiExceptionResponse> handleGenericException(Exception exception) {
+        return buildResponse(HttpStatus.INTERNAL_SERVER_ERROR, Collections.singletonList("Internal Server error: " + exception.getMessage()));
+    }
+
+    private ResponseEntity<ApiExceptionResponse> buildResponse(HttpStatus statusCode, List<String> messages) {
         ApiExceptionResponse response = ApiExceptionResponse.builder()
-                .errorMessages(errorMessages)
-                .code(statusCode)
+                .code(statusCode.value())
+                .errorMessages(messages)
                 .build();
-        return ResponseEntity.status(exception.getHttpStatus()).body(response);
+        return ResponseEntity.status(statusCode).body(response);
     }
 }
