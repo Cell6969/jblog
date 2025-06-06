132e03c12360b203b3859ae3f2051ff229421acf
Cell6969 <bossmarinoo@gmail.com>
2025-06-06 16:32:47 +0700

    add: auth controller

---
+++ src/main/java/com/fcidn/blog/controller/AuthController.java
@@ -0,0 +1,48 @@
+package com.fcidn.blog.controller;
+
+import com.fcidn.blog.helper.ApiResponse;
+import com.fcidn.blog.helper.ResponseHelper;
+import com.fcidn.blog.request.auth.LoginRequest;
+import com.fcidn.blog.response.auth.LoginResponse;
+import com.fcidn.blog.service.JwtService;
+import com.fcidn.blog.service.MyUserDetailService;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AuthController {
+    @Autowired
+    JwtService jwtService;
+
+    @Autowired
+    AuthenticationManager authenticationManager;
+
+    @Autowired
+    MyUserDetailService myUserDetailService;
+
+    @PostMapping("/api/login")
+    public ResponseEntity<ApiResponse<LoginResponse>> login(@Valid @RequestBody LoginRequest request) {
+        Authentication authentication = authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(request.getUsername(), request.getPassword())
+        );
+
+        if (authentication.isAuthenticated()) {
+            String token = jwtService.generateToken(myUserDetailService.loadUserByUsername(request.getUsername()));
+            LoginResponse response = LoginResponse.builder()
+                    .token(token)
+                    .build();
+            return ResponseHelper.response(response, HttpStatus.OK, "Successfully login");
+        }
+
+        throw new UsernameNotFoundException("user not found");
+    }
+}
