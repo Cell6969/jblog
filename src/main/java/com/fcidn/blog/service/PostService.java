5d740a681617c97001f411004acca77a2f6188dd
Cell6969 <bossmarinoo@gmail.com>
2025-06-17 14:56:16 +0700

    update: implement cache on post service

--- src/main/java/com/fcidn/blog/service/PostService.java
+++ src/main/java/com/fcidn/blog/service/PostService.java
@@ -16,6 +16,9 @@ import com.fcidn.blog.response.post.UpdatePostResponse;
 import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.cache.annotation.Caching;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
@@ -36,18 +39,22 @@ public class PostService {
     @Autowired
     PostMapper postMapper;
 
+    @Cacheable(value = "PostService.getPosts", key = "{#isPublished, #isDeleted, #request.page}")
     public Iterable<GetPostResponse> getPosts(GetPostRequest request, Boolean isPublished, Boolean isDeleted) {
         PageRequest pageRequest = PageRequest.of(request.getPage(), request.getLimit());
-        List<Post> posts = postRepository.findAllByIsDeletedAndIsPublished(isDeleted, isPublished ,pageRequest).getContent();
+        List<Post> posts = postRepository.findAllByIsDeletedAndIsPublished(isDeleted, isPublished, pageRequest)
+                .getContent();
         return postMapper.mapToListPost(posts);
     }
 
+    @Cacheable(value = "PostService.getPostBySlug", key = "#request.slug")
     public GetPostResponse getPostBySlug(GetPostBySlugRequest request, Boolean isDeleted) {
-        Post post =  postRepository.findFirstBySlugAndIsDeleted(request.getSlug(), isDeleted)
+        Post post = postRepository.findFirstBySlugAndIsDeleted(request.getSlug(), isDeleted)
                 .orElseThrow(() -> new ApiException("not found", HttpStatus.NOT_FOUND));
         return postMapper.mapToGetPost(post);
     }
 
+    @CacheEvict(value = "PostService.getPosts", allEntries = true)
     public CreatePostResponse createPost(CreatePostRequest createPostRequest) {
         String categorySlug = createPostRequest.getCategory_name().toLowerCase();
         Category category = categoryRepository.findFirstBySlug(categorySlug)
@@ -60,6 +67,13 @@ public class PostService {
         return postMapper.mapToCreatePost(post);
     }
 
+    // @CachePut(value = "PostService.getPostBySlug", key = "#request.slug")
+    @Caching(
+        evict = {
+            @CacheEvict(value = "PostService.getPostBySlug", allEntries = true),
+            @CacheEvict(value = "PostService.getPosts", allEntries = true)
+        }
+    )
     public UpdatePostResponse updatePostById(Integer id, UpdatePostRequest request) {
         Post post = postRepository
                 .findFirstByIdAndIsDeleted(id, false)
@@ -73,18 +87,32 @@ public class PostService {
         return postMapper.mapToUpdatePost(updatedPost);
     }
 
+    @Caching(
+        evict = {
+            @CacheEvict(value = "PostService.getPostBySlug", allEntries = true),
+            @CacheEvict(value = "PostService.getPosts", allEntries = true)
+        }
+    )
     public Boolean deletePostById(Integer id) {
-        Post post = postRepository.findById(id).orElseThrow(() -> new ApiException("post not found", HttpStatus.NOT_FOUND));
+        Post post = postRepository.findById(id)
+                .orElseThrow(() -> new ApiException("post not found", HttpStatus.NOT_FOUND));
         post.setDeleted(true);
         postRepository.save(post);
         return true;
     }
 
+    @Caching(
+        evict = {
+            @CacheEvict(value = "PostService.getPostBySlug", allEntries = true),
+            @CacheEvict(value = "PostService.getPosts", allEntries = true)
+        }
+    )
     public GetPostResponse publishPost(Integer id) {
-        Post post = postRepository.findById(id).orElseThrow(() -> new ApiException("post not found", HttpStatus.NOT_FOUND));
+        Post post = postRepository.findById(id)
+                .orElseThrow(() -> new ApiException("post not found", HttpStatus.NOT_FOUND));
         post.setPublished(true);
         post.setPublishedAt(Instant.now().getEpochSecond());
-        post =  postRepository.save(post);
+        post = postRepository.save(post);
         return postMapper.mapToGetPost(post);
     }
 }
